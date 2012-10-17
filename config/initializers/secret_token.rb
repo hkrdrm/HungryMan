ac577ee51f30ddd4d4b0d664def3665e3c290768
Marble.Cake <hkr.drm@gmx.com>
2012-10-16 20:22:30 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HungryMan::Application.config.secret_token = '17b7b6f36bed6c349245e1410c57217a388ee0a771d0f7781a187f3c4b63ced4eafd0de6ec6233065204a2918e505fcb7778509aadf9c61cc2f33ecda8281774'
